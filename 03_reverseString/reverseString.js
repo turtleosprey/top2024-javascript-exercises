510a410da0582b7d8f32fd13c1c3d9e88c77c5ee
turtleosprey <182770837+turtleosprey@users.noreply.github.com>
2024-10-20 19:18:08 +0800

    solve: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+  output = word.split("");
+
+  let popped = ``
+
+  for (i = output.length - 1; i >= 0; i--){
+    popped += output.pop()
+  }
+
+  // for (char of output) {
+  //   popped += output.pop()
+  // }
+
+  return popped
+
+  // output = word.split("").join("")
+  // return output
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// npm test reverseString.spec.js     
