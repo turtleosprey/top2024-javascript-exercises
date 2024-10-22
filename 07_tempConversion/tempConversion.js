c50c8eb8caefb83ffdbacea6f5e9382286da2705
turtleosprey <182770837+turtleosprey@users.noreply.github.com>
2024-10-22 20:37:35 +0800

    solve: 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  degree = (degree-32)*5/9
+  return +Number.parseFloat(degree).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  degree = (degree * 9/5) + 32
+  return +Number.parseFloat(degree).toFixed(1)
 };
 
 // Do not edit below this line
@@ -9,3 +13,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// npm test tempConversion.spec.js
