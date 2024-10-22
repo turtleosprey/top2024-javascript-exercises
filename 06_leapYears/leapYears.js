9475ddfe8058e95d7fdb6a1fbbfd78a717c0a533
turtleosprey <182770837+turtleosprey@users.noreply.github.com>
2024-10-22 19:18:15 +0800

    solve: 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  
+  if (year % 100 == 0 && year % 400 == 0) {year = true}
+  else if (year % 4 == 0)
+    {if(year % 100 == 0){year = false} else year = true}
+  else {year = false}
 
+  return year
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// npm test leapYears.spec.js
