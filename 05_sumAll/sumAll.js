922f6e1f2a7c2022a91836d6a67faa554aa8004d
turtleosprey <182770837+turtleosprey@users.noreply.github.com>
2024-10-22 18:44:39 +0800

    solve: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let sum = 0;
 
+  let firstNumber = args[0]
+  let lastNumber = args[args.length - 1]
+
+  if (Number.isInteger(firstNumber) && Number.isInteger(lastNumber) && !Number.isNaN(firstNumber) && !Number.isNaN(lastNumber) && !(firstNumber < 0) && !(lastNumber < 0)){
+
+    if (firstNumber < lastNumber){
+      for (a = firstNumber; a <= lastNumber; a++){
+        sum += a
+      }
+    }
+    
+    if (firstNumber > lastNumber){
+      for (a = firstNumber; a >= lastNumber; a--){
+        sum += a
+      }
+    }
+
+  } else {
+    sum = 'ERROR'
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// npm test 05_sumAll/sumAll.spec.js
